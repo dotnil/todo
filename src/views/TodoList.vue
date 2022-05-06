80ba397e9ee41175c5f1c8912c3ab755e723c955
Tatiana Kh <ts.khol@gmail.com>
2022-05-06 22:16:00 +0300

    add views/TodoList.vue

---
+++ src/views/TodoList.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+.todo-list__wrapper
+  h1.todo-list__header Todo
+
+  ul.todo-list__container
+    li.todo-list__item function
+    li.todo-list__item take a rest
+
+  input.todo-list__new-item
+  .todo-list__button
+    a(class='todo-list__save' href='#') Save
+</template>
+
+<script setup>
+</script>
+
+<style>
+</style>
