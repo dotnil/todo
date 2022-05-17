281807884ed4f39a5aa04d695c6048c93a1e3bb5
Tatiana Kh <ts.khol@gmail.com>
2022-05-17 23:37:40 +0300

    add props to TodoItem.vue

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,10 +1,11 @@
 <template lang='pug'>
 li.todo-item
-    span.todo-item__name
-    a.todo-item__delete(href='#') ×
+  span.todo-item__name(:class="{ 'todo-item__name_completed': todo.done }" @click='toggleTask') {{ todo.name }}
+  a.todo-item__delete(href='#' @click='deleteTask') &times;
 </template>
 
 <script setup>
-import TodoItem from '@/components/TodoItem.vue'
-
+const props = defineProps({
+  todo: { type: Object, default: () => ({name: 'task', done: false}) },
+})
 </script>
--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -3,15 +3,23 @@
   h1.todo-list__header Todo
 
   ul.todo-list__container
-    TodoItem
+  TodoItem(
+    v-for="(todo, index) in todos"
+    :key="index"
+    :todo="todo"
+    @deleteTask="deleteTask"
+    @toggleTask="toggleTask"
+  )
   input.todo-list__new-item
   .todo-list__button
     a(class='todo-list__save' href='#') Save
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import TodoItem from '@/components/TodoItem.vue'
 
+const todos = ref([])
 </script>
 
 <style>
